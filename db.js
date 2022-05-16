66d614836d33e45b7e6995d7c502604e423fbc01
Reese0177 <vinnygo@gmail.com>
2022-05-15 20:04:48 -0500

    Part 2 done

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
 // TODO - create the new sequelize connection
-const sequelize = new Sequelize({dialect: 'sqlite', storage: "./db.sqlite"});
+const sequelize = new Sequelize({dialect: 'sqlite', storage: "./db.sqlite", logging: false});
 module.exports = {
     sequelize,
     Sequelize,
--- index.test.js
+++ index.test.js
@@ -20,7 +20,21 @@ describe('Band and Musician Models', () => {
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
-        const newMusician = await Musician.create({name: "Mr. Music Man", instrument: "guitar"});
+        const newMusician = await Musician.create({name: "Mr. Music Man", instrument: "Guitar"});
         expect(newMusician.name).toBe('Mr. Music Man');
+    })
+
+    test('associate musicians to band', async () => {
+        const newBand = await Band.create({name: 'Bandy', genre: 'Rock'});
+        const newMusician2 = await Musician.create({name: "Drummer Boy", instrument:"Drums"});
+        const newMusician1 = await Musician.create({name: "Mr. Music Man", instrument: "Guitar"});
+
+        await newBand.addMusician(newMusician1);
+        await newBand.addMusician(newMusician2);
+
+        const musicians = await newBand.getMusicians();
+
+        expect(musicians.length).toBe(2);
+        expect(musicians[0] instanceof Musician).toBeTruthy();
     })
 })
